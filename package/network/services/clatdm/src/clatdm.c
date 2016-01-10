b266e597d19053a3e2275a41afd09a47c73467c4
zhaoxin54430 <zhaoxin54430@163.com>
2016-01-10 22:47:21 +0800

    modify clatdm, not verification function

--- package/network/services/clatdm/src/clatdm.c
+++ package/network/services/clatdm/src/clatdm.c
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2012, Stéphan Kochen <stephan@kochen.nl>
+ * Copyright 2012, St鑼卲han Kochen <stephan@kochen.nl>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -16,20 +16,95 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+    
+#include <stdlib.h>  
 #include <stdio.h>  
-#include <time.h>  
+#include <string.h>  
+#include <time.h>
+#include <syslog.h>
+#include <errno.h>
+#include <unistd.h>
+#include <libshmem.h>
+#include <signal.h>
+#include <stdbool.h>
+#include "clatdm.h"
 
-/*
-struct timespec {
-time_t tv_sec; // seconds
-long tv_nsec; // and nanoseconds
-};
-*/
-int main()  
+
+all_client_info *shm_ptr=NULL;
+
+int main(int argc, char **argv)
 {  
-    struct timespec time1 = {0, 0};  
-    clock_gettime(CLOCK_MONOTONIC, &time1);  
-    printf("CLOCK_MONOTONIC: %d, %d", time1.tv_sec, time1.tv_nsec);  
-    printf("\n%d\n", time(NULL));  
-    sleep(1);  
+    void *ptr=NULL;
+    client_info client;
+
+    signal(SIGPIPE, SIG_IGN);
+    openlog("clatdm", LOG_NDELAY , LOG_USER);
+
+    shm_ptr=NULL;
+    if(Lock_init(false)!=SHMRET_SUCCESS)
+    {
+        clatdm_error("Lock_init failed, errno=%d", errno);
+        closelog();
+        exit(1);
+    }
+    /*only management process init sem*/
+    if(init_sem()!=SHMRET_SUCCESS)
+    {
+        clatdm_error("init_sem failed, errno=%d", errno);
+        goto quit_lock;
+    }
+    if(shm_mem_init(false)!=SHMRET_SUCCESS)
+    {
+        clatdm_error("shm_mem_init failed, errno=%d", errno);
+        goto quit_lock;
+    }
+    ptr=shm_mem_attach();
+    if(ptr==(void *)(-1))
+    {
+        clatdm_error("shm_mem_attach failed, errno=%d", errno);
+        goto quit_mem;
+    }
+    shm_ptr=(all_client_info *)ptr;
+    
+    sem_lock();
+    /*only management process init the number*/
+    shm_ptr->client_num=0;
+    sem_unlock();
+
+    char lastnum=0;
+    bool isAdd=false;
+    while(true)
+    {
+        sleep(120);
+        sem_lock();
+        if(shm_ptr->client_num > lastnum)
+        {
+            memcpy(&client, &(shm_ptr->client[shm_ptr->client_num-1]), sizeof(client_info));
+            lastnum=shm_ptr->client_num;
+            isAdd=true;
+        }
+        sem_unlock();
+        if(isAdd)
+        {
+            clatdm_error("add new client to share memory:%X", client.ip4_addr);
+            isAdd=false;
+        }
+    }
+    shm_mem_detach(ptr);
+    shm_ptr=NULL;
+    shm_mem_cleanup();
+    Lock_cleanup();
+    closelog();
+    exit(0);
+
+quit_lock:
+    Lock_cleanup();
+    closelog();
+    exit(1);
+    
+quit_mem:
+    shm_mem_cleanup();
+    Lock_cleanup();
+    closelog();
+    exit(1);    
 } 
---
+++ package/network/services/clatdm/src/clatdm.h
@@ -0,0 +1,11 @@
+#include <syslog.h>
+
+#define CLATDM_OUTPUT_TO_SYSLOG
+
+#ifdef CLATDM_OUTPUT_TO_SYSLOG
+#define clatdm_error(args...) syslog(LOG_ERR, args);
+#define clatdm_info(args...) syslog(LOG_INFO, args);
+#else
+#define clatdm_error(args...)
+#define clatdm_info(args...)
+#endif
