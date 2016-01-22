414f9ff418e89dfa107129da399f9203afdd9b8d
xin.zhao.sz <xin.zhao.sz@jrdcom.com>
2016-01-22 18:53:28 +0800

    add libcurl to clatdm

--- package/network/services/clatdm/src/clatdm.c
+++ package/network/services/clatdm/src/clatdm.c
@@ -27,17 +27,118 @@
 #include <libshmem.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <curl/curl.h>
 #include "clatdm.h"
+#include "others.h"
 
+#define CHECK_AUTH_TIMEOUT  180  //seconds
+#define AUTH_SUCCESS_TIMEOUT  43200 //seconds 12 hours
+#define WAN_INTERFACE_NAME "eth0.2"
+#define CURL_PERFORM_TIMEOUT 30 //seconds
 
 all_client_info *shm_ptr=NULL;
+static bool wan_hwaddr_valid=false;
+static char wan_hwaddr[6];
+static CURL *curl_handle=NULL;
 
+bool get_wan_hwaddr(char *wan_hwaddr)
+{
+    int fd=-1;
+    bool success=false;
+    struct ifreq ifr;
+    
+    if(!wan_hwaddr)
+        return success;
+        
+    if ((fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) >= 0) 
+    {
+        strcpy(ifr.ifr_name, WAN_INTERFACE_NAME);
+        if (ioctl(fd, SIOCGIFHWADDR, &ifr) == 0) 
+        {
+            memcpy(wan_hwaddr, ifr.ifr_hwaddr.sa_data, 6);
+            clatdm_info("wan hardware address %02x:%02x:%02x:%02x:%02x:%02x",
+            	0xFF&wan_hwaddr[0], 0xFF&wan_hwaddr[1], 0xFF&wan_hwaddr[2], 
+            	0xFF&wan_hwaddr[3], 0xFF&wan_hwaddr[4], 0xFF&wan_hwaddr[5]);
+            success=true;
+        } 
+        else 
+        {
+            clatdm_info("SIOCGIFHWADDR failed! %s", strerror(errno));
+        }
+    } 
+    else
+    {
+        clatdm_info("socket failed! %s", strerror(errno));
+    }
+    close(fd);
+    return success;
+}
+
+void curl_init(void)
+{
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+    if(!curl_handle)
+    {
+        clatdm_error("curl_easy_init failed");
+    }
+    return;
+}
+size_t get_write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+    size_t all_size=nmemb*size;
+    char buf[64];
+    
+    //clatdm_error("get response data:%s", ptr);
+    if(all_size<(sizeof(buf)-1))
+    {
+        memset(buf, ptr, all_size);
+        buf[all_size]=0;
+        clatdm_error("get response data:%s", buf);
+    }
+    clatdm_error("get response size:%d", all_size);
+    return(all_size);
+}
+CURLcode curl_get_init(char *url)
+{
+    CURLcode res=CURLE_HTTP_RETURNED_ERROR;
+    if(curl_handle)
+    {
+        res=curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+        if(res != CURLE_OK)
+        {
+            clatdm_error("CURLOPT_URL error");
+            return res;
+        }
+        res=curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, get_write_callback);
+        if(res != CURLE_OK)
+        {
+            clatdm_error("CURLOPT_WRITEFUNCTION error");
+            return res;
+        }
+        res=curl_easy_setopt(curl_handle, CURLOPT_TIMEOUT, 30);
+        if(res != CURLE_OK)
+        {
+            clatdm_error("CURLOPT_TIMEOUT error");
+            return res;
+        }
+    }
+    return res;
+}
 int main(int argc, char **argv)
 {  
     void *ptr=NULL;
     client_info client;
     char attachExisting=false;
     FILE *flag_file=NULL;
+    int i=0;
+    time_t temp_time=0;
+    CURLcode res;
 
     if(access(SHARE_MEM_FLAG, F_OK)==0)
         attachExisting=true;
@@ -87,7 +188,7 @@ int main(int argc, char **argv)
     if(ptr==(void *)(-1))
     {
         clatdm_error("shm_mem_attach failed, errno=%d", errno);
-        goto quit_mem;
+        goto quit_lock;
     }
     shm_ptr=(all_client_info *)ptr;
     
@@ -99,27 +200,53 @@ int main(int argc, char **argv)
         sem_unlock();
     }
 
-    char lastnum=0;
-    bool isAdd=false;
+    if(get_wan_hwaddr(wan_hwaddr))
+    {
+        wan_hwaddr_valid=true;
+    }
+    else
+    {
+        clatdm_error("get_wan_hwaddr failed");
+        goto quit_mem;
+    }
+//clatdm_error("sizeof client_info %d", sizeof(client_info));    
+    curl_init();
+    if(curl_get_init(CLATDM_PATH)!=CURLE_OK)
+    {
+        if(curl_handle)
+            curl_easy_cleanup(curl_handle);
+            
+        curl_global_cleanup();
+        goto quit_mem;
+    }
+    res = curl_easy_perform(curl_handle);
+    if(res != CURLE_OK)
+        clatdm_error("curl perform failed %s", curl_easy_strerror(res));
+
+    long response_code;
+    if(curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &response_code)==CURLE_OK)
+    {
+        clatdm_error("curl perform response code %d", response_code);
+    }
     while(true)
     {
-        sleep(120);
+#if 0    
         sem_lock();
-        if(shm_ptr->client_num > lastnum)
+        for(i=0; i<shm_ptr->client_num; i++)
         {
-            memcpy(&client, &(shm_ptr->client[shm_ptr->client_num-1]), sizeof(client_info));
-            lastnum=shm_ptr->client_num;
-            isAdd=true;
+            memcpy(&client, &(shm_ptr->client[i]), sizeof(client_info));
         }
         sem_unlock();
-        if(isAdd)
-        {
-            clatdm_error("add new client to share memory:%X", client.ip4_addr);
-            isAdd=false;
-        }
+#else
+    sleep(180);
+#endif
     }
     shm_mem_detach(ptr);
     shm_ptr=NULL;
+    
+    if(curl_handle)
+        curl_easy_cleanup(curl_handle);
+    curl_global_cleanup();
     //shm_mem_cleanup();
     //Lock_cleanup();
     closelog();
@@ -131,6 +258,7 @@ quit_lock:
     exit(1);
     
 quit_mem:
+    shm_mem_detach(ptr);
     //shm_mem_cleanup();
     //Lock_cleanup();
     closelog();
