fb365d9265423a37e169fdb6328a3b4ad6a8a0d9
zhaoxin54430 <zhaoxin54430@163.com>
2016-03-12 11:38:08 +0800

    modify wifi ssid length limit

--- src_luci/modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi_conf.lua
+++ src_luci/modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi_conf.lua
@@ -3,21 +3,22 @@
 
 local nw = require "luci.model.network"
 
-m = Map("wireless", "", translate("The Configuration page of the wifi, The first is the master wifi interface"))
+m = Map("wireless", "", translate("Wifi configuration page"))
 
 s = m:section(TypedSection, "wifi-iface", translate("Interface Configuration"))
 s.addremove = false
 s.anonymous = true
 
-ssid_name = s:option(Value, "ssid", translate("wifi ssid"))
-ssid_name.maxlength = 28
-ssid_name.size = 28
-ssid_name.rmempty = true
+ssid_name = s:option(Value, "ssid", translate("Wifi ssid (3-30)characters, One chinese as 3 characters"))
+--ssid_name.datatype = "rangelength(8, 28)"
+ssid_name.rmempty = false
 
-function ssid_name.validate(self, value)
-    if value == nil or value == false then
-        return nil, translate("ssid can't empty!")
-    end 
+function ssid_name.validate(self, value, section)
+    local length = string.len(value)
+	local err_str = translate("The length should be (3-30)characters, current is ") .. length
+    if length < 3 or length > 30 then
+        return nil, err_str
+    end
     return value
 end
 
