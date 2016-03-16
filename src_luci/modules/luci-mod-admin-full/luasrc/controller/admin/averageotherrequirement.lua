300bc103f38b51751867e20175711ab508bdd922
xin.zhao.sz <xin.zhao.sz@jrdcom.com>
2016-03-16 10:56:11 +0800

    modify upgrade res dir

--- src_luci/modules/luci-mod-admin-full/luasrc/controller/admin/averageotherrequirement.lua
+++ src_luci/modules/luci-mod-admin-full/luasrc/controller/admin/averageotherrequirement.lua
@@ -24,16 +24,16 @@ function action_requirement()
 	local sys = require "luci.sys"
 	local fs  = require "nixio.fs"
 	local resource_dir   = "/tmp/others_res/"
-	local resource_tmp   = resource_dir .. "res.tar.gz"
-	local resource_res   = resource_dir .. "res/*"
+	local resource_tmp   = resource_dir .. "webs.tar.gz"
+	local resource_res   = resource_dir .. "webs"
 	local pass_file = "/etc/upgrade_o_pass"
 	local res_dir = "/www/connect/res/"
-	local res_dir_all = "/www/connect/res/*"
+	local res_dir_webs = "/www/connect/res/webs"
 	local result = false
 	local local_pass = nil
 
 	local function file_supported()         
-		return (os.execute("tar -zxvf %s -C %s >/dev/null" %{resource_tmp , resource_dir}) == 0)        
+		return (os.execute("tar -zxf %s -C %s >/dev/null" %{resource_tmp , resource_dir}) == 0)        
 	end
 	local fp
 	luci.http.setfilehandler(
@@ -58,9 +58,9 @@ function action_requirement()
 		local luci_pass = luci.http.formvalue("up_password")
 		if local_pass and luci_pass and luci_pass == local_pass and file_supported() then
 			os.execute("rm -rf %s >/dev/null" % resource_tmp)
-			os.execute("rm -rf %s >/dev/null" % res_dir_all)
+			os.execute("rm -rf %s >/dev/null" % res_dir_webs)
 			if os.execute("mv -f %s %s >/dev/null" %{resource_res , res_dir}) == 0 then
-				os.execute("chmod 644 -R %s >/dev/null", % res_dir)
+				os.execute("chmod 644 -R %s >/dev/null" % res_dir_webs)
 				result = true
 			end
 		end
