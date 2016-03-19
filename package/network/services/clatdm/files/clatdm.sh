15317e3ad8963e386610d88907775689da0e047c
zhaoxin54430 <zhaoxin54430@163.com>
2016-03-19 15:39:10 +0800

    modify can't mount mtd7 after uprade firmware

--- package/network/services/clatdm/files/clatdm.sh
+++ package/network/services/clatdm/files/clatdm.sh
@@ -5,39 +5,7 @@ USE_PROCD=1
 START=45
 
 
-MTDBLOCK7="mtdblock7"
-OTHER_ROOT="/www/connect/res"
-OTHER_ROOT_IMG="/www/connect/other.img"
-OTHER_ROOT_FLAG="/www/connect/res/other_data_file_root_dir"
-OTHER_DETECT_FILE="/www/connect/res/m_o_s_f"
-MTD_PARTION_NAME="/dev/mtd7"
-
 start_service() {
-	
-	local mount_str
-	
-	mount_str=`mount | grep $MTDBLOCK7`
-	[ -z "$mount_str" ] && {
-	# not mount
-		[ -d "$OTHER_ROOT" ] || {
-			mkdir $OTHER_ROOT
-		}
-		mount -t jffs2 /dev/$MTDBLOCK7 $OTHER_ROOT -o rw,sync
-		[ -f "$OTHER_ROOT_FLAG" ] || {
-			[ -f "$OTHER_DETECT_FILE" ] || {
-			#file system invalid
-				umount $OTHER_ROOT
-				mtd erase $MTD_PARTION_NAME
-				mtd write $OTHER_ROOT_IMG $MTD_PARTION_NAME
-				mount -t jffs2 /dev/$MTDBLOCK7 $OTHER_ROOT -o rw,sync
-				touch $OTHER_DETECT_FILE
-			}
-			[ -f "$OTHER_ROOT_FLAG" ] || {
-				touch $OTHER_ROOT_FLAG
-			}
-		}
-	}
-	mkdir /tmp/others_res
 	
   iptables -nvL FORWARD | grep -q '#conn' || {
           iptables -I FORWARD 1 -p tcp -m connlimit --connlimit-above 128 -j DROP
